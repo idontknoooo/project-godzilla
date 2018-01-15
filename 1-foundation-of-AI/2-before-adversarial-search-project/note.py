b37a2e3471f7a527ff05c98f1467328728e26c4c
idontknoooo <jordan50zuo@gmail.com>
2018-01-15 15:36:17 -0500

    chore: Add more notes Add notes before second project

--- 1-foundation-of-AI/2-before-adversarial-search-project/note.py
+++ 1-foundation-of-AI/2-before-adversarial-search-project/note.py
@@ -7,6 +7,7 @@
 # 5. Isolation Game Player
 # 6. Multi-player, probabilistic Games
 
+# LESSON 8: Introduction to Game Playing
 # 1. Isolation: Unable to move on their turn (in a game)
 # 2. Minimax Algorithm: detech bad moves  
 #   a. Top of Minimax tree will always be a Max level, since we are trying to build a smart machine, and always start from machine's perspective.
@@ -21,3 +22,21 @@
 # 6. Quiescent Search: Search to a depth for a few nodes (not all) where the recommendation will change much any more. 
 #       This is a remedy method to solve "horizon effect", which meaning computing power cannot do fully search for all nodes at the same level.
 #       Instead, quiescent search, do deeper search on a few "interested nodes" instead of those quiet ones.
+
+# LESSON 9: Advanced Game Playing
+# 1. Iterative Deepening DFS (IDS)
+# https://www.cs.ubc.ca/~hutter/teaching/cpsc322/2-Search6-final.pdf
+#   a. Search time is dominated by last level search
+#   b. Time: O(b^d) b: branching factor, d: depth
+#   c. Space: O(db)
+#   d. n = (b^(d+1)-1)/(b-1), n: number of nodes in the tree
+# 2. Horizon Effect: Something obvious for human but difficult for computer due to the search is not deep enough
+# 3. Evaluating Evaluation Function:
+#   a. #my_moves - #opponents_moves
+#   b. #my-moves - 2 * #opponents_moves
+# 4. Alpha-Beta Pruning
+#   a. Alpha: The max lower bound of minimax value
+#   b. Beta: The minimum upper bound of the minimax value
+#   c. Prune: Omit the process of evaluating, reorder nodes can make more nodes available for pruning
+# 5. AI: Clever solutions to Exponential Problems
+# 6. 3-Players Max-Max-Max Pruning
