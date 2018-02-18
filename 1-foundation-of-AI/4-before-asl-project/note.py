ec177db390d4ca219ebcd643fc4fcb95e4756d65
idontknoooo <jordan50zuo@gmail.com>
2018-02-18 00:33:38 -0500

    chore: add more notes

---
+++ 1-foundation-of-AI/4-before-asl-project/note.py
@@ -0,0 +1,17 @@
+# LESSON 17: Bayes Nets
+# 1. Bayes Network: 
+#   1. Need 2 variables (A & B). 
+#   2. Know P(A) & P(B|A) & P(B|!A)
+#   3. Want to know P(A|B) & P(A|!B)
+#   4. All above is called entire network of Bayes net
+# 2. Bayes Rule
+#   P(A|B) = P(B|A)P(A) / P(B)
+#   P(!A|B) = P(B|!A)P(!A) / P(B)
+#   P(A|B) + P(!A|B) = 1
+# 3. Other Probability Rules:
+#   P(AB) = P(A|B)P(B) = P(B|A)P(A) 
+# 4. Conditional Independence
+#   P(AB|T) = P(A|T)*P(B|T) 
+#   Conditional Indenpendence != Absolute Independence, they don't imply each other either
+# 5. Different Type Of Bayes Network
+# 6. D Separation 
